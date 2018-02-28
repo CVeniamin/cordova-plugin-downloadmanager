b5a6eeb3e5154ef5f32c20a5425f6baa180366ab
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2018-02-28 01:17:28 +0000

    use public directory

--- src/android/DownloadManager.java
+++ src/android/DownloadManager.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
 
+import android.app.DownloadManager;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -21,76 +23,78 @@ import java.util.Calendar;
 /**
  * This class echoes a string called from JavaScript.
  */
+
 public class DownloadManager extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("download")) {
-            final JSONObject params = args.getJSONObject(0);
-            this.startDownload(params, callbackContext);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	if (action.equals("download")) {
+		final JSONObject params = args.getJSONObject(0);
+		this.startDownload(params, callbackContext);
+		return true;
+	}
+	return false;
+}
 
-    private void startDownload(final JSONObject options, CallbackContext callbackContext) {
-        if (options != null && options.length() > 0) {
-            
-	    //default filename
-            DateFormat df = new SimpleDateFormat("yyMMddHHmmss.SSS");
-            String filename = df.format(Calendar.getInstance().getTime());
-	    String uri;
-			
-            if (options.has("uri")) {
-                uri = options.getString("uri");
-                filename = uri.substring(uri.lastIndexOf("/")+1, uri.length());
-            }
-           
-            try {
-                filename = URLDecoder.decode(filename,"UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                callbackContext.error("Error in converting filename");
-            }
-			
-            android.app.DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
-            Uri Download_Uri = Uri.parse(uri);
-            
-            android.app.DownloadManager.Request request = new android.app.DownloadManager.Request(Download_Uri);
-            
-            //Restrict the types of networks over which this download may proceed.
-            request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
-			
-            //Set whether this download may proceed over a roaming connection.
-            request.setAllowedOverRoaming(false);
-			
- 	    //Set the title of this download, to be displayed in notifications (if enabled).
-		if(options.has("title")){
-			request.setTitle(options.getString("title"));
+private void startDownload(final JSONObject options, CallbackContext callbackContext) {
+	try {
+		if (options != null && options.length() > 0) {
+			//default filename
+			DateFormat df = new SimpleDateFormat("yyMMddHHmmss.SSS");
+			String filename = df.format(Calendar.getInstance().getTime());
+			String uri;
+			if (options.has("uri")){
+				uri = options.getString("uri");
+				filename = uri.substring(uri.lastIndexOf("/")+1, uri.length());
+
+				try {
+					filename = URLDecoder.decode(filename,"UTF-8");
+				} catch (UnsupportedEncodingException e) {
+					callbackContext.error("Error in converting filename");
+				}
+
+				DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
+				Uri Download_Uri = Uri.parse(uri);
+
+				DownloadManager.Request request = new android.app.DownloadManager.Request(Download_Uri);
+
+				//Restrict the types of networks over which this download may proceed.
+				request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
+
+				//Set whether this download may proceed over a roaming connection.
+				request.setAllowedOverRoaming(false);
+
+				//Set the title of this download, to be displayed in notifications (if enabled).
+				if(options.has("title")){
+					request.setTitle(options.getString("title"));
+				} else {
+					request.setTitle(filename);
+				}
+
+				//Set a description of this download, to be displayed in notifications (if enabled)
+				if(options.has("description")){
+					request.setDescription(options.getString("description"));
+				} else {
+					request.setDescription("Downloading file");
+				}
+
+				//Set the local destination for the downloaded file to a path within the application's external files directory
+				if(options.has("setPublicDirectory") && options.has("albumName") && options.getBoolean("setPublicDirectory") && options.getString("albumName").length() > 0 ) {
+					request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "/" + options.getString("albumName") + "/" + filename);
+				} else {
+					//Set the local destination for the downloaded file to a path within the application's external files directory
+					request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, filename);
+				}
+
+				//Set visiblity after download is complete
+				request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+				long downloadReference = downloadManager.enqueue(request);
+				callbackContext.success(filename);
+			}
 		} else {
-			request.setTitle(filename);
-	    }
-			
-	    //Set a description of this download, to be displayed in notifications (if enabled)
-	    if(options.has("description")){
-			request.setDescription(options.getString("description"));   
-	    } else {
-			request.setDescription("Downloading file");   
-	    }
-			
-		//Set the local destination for the downloaded file to a path within the application's external files directory            
-		if(options.has("setPublicDirectory") && options.has("albumName") && options.getBoolean("setPublicDirectory") && options.getString("albumName").length() > 0 ) {
-			request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_PICTURES, options.getString("albumName"));
-	    } else {
-			//Set the local destination for the downloaded file to a path within the application's external files directory
-			request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, filename);   
-	    }
-
-		//Set visiblity after download is complete
-		request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-		long downloadReference = downloadManager.enqueue(request);
-		callbackContext.success(filename);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+			callbackContext.error("Expected one non-empty string argument.");
+		}
+	} catch (JSONException e) {
+		callbackContext.error(e.getMessage());
+	}
 }

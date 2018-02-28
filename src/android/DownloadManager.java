6993627f32063129e76c02166214b852b9dba5c5
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2018-02-28 01:22:52 +0000

    Update DownloadManager.java

--- src/android/DownloadManager.java
+++ src/android/DownloadManager.java
@@ -32,7 +32,7 @@ public class DownloadManager extends CordovaPlugin {
 			final JSONObject params = args.getJSONObject(0);
 			this.startDownload(params, callbackContext);
 			return true;
-		}   
+		}
 		return false;
 	}
 
@@ -46,38 +46,38 @@ public class DownloadManager extends CordovaPlugin {
 				if (options.has("uri")){
 					uri = options.getString("uri");
 					filename = uri.substring(uri.lastIndexOf("/")+1, uri.length());
-	
+
 					try {
 						filename = URLDecoder.decode(filename,"UTF-8");
 					} catch (UnsupportedEncodingException e) {
 						callbackContext.error("Error in converting filename");
 					}
-	
-					DownloadManager downloadManager = (android.app.DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
+
+					DownloadManager downloadManager = (DownloadManager) cordova.getActivity().getApplicationContext().getSystemService(Context.DOWNLOAD_SERVICE);
 					Uri Download_Uri = Uri.parse(uri);
-	
-					DownloadManager.Request request = new android.app.DownloadManager.Request(Download_Uri);
-	
+
+					DownloadManager.Request request = new DownloadManager.Request(Download_Uri);
+
 					//Restrict the types of networks over which this download may proceed.
-					request.setAllowedNetworkTypes(android.app.DownloadManager.Request.NETWORK_WIFI | android.app.DownloadManager.Request.NETWORK_MOBILE);
-	
+					request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+
 					//Set whether this download may proceed over a roaming connection.
 					request.setAllowedOverRoaming(false);
-	
+
 					//Set the title of this download, to be displayed in notifications (if enabled).
 					if(options.has("title")){
 						request.setTitle(options.getString("title"));
 					} else {
 						request.setTitle(filename);
 					}
-	
+
 					//Set a description of this download, to be displayed in notifications (if enabled)
 					if(options.has("description")){
 						request.setDescription(options.getString("description"));
 					} else {
 						request.setDescription("Downloading file");
 					}
-	
+
 					//Set the local destination for the downloaded file to a path within the application's external files directory
 					if(options.has("setPublicDirectory") && options.has("albumName") && options.getBoolean("setPublicDirectory") && options.getString("albumName").length() > 0 ) {
 						request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, "/" + options.getString("albumName") + "/" + filename);
@@ -85,9 +85,9 @@ public class DownloadManager extends CordovaPlugin {
 						//Set the local destination for the downloaded file to a path within the application's external files directory
 						request.setDestinationInExternalFilesDir(cordova.getActivity().getApplicationContext(), Environment.DIRECTORY_DOWNLOADS, filename);
 					}
-	
+
 					//Set visiblity after download is complete
-					request.setNotificationVisibility(android.app.DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+					request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 					long downloadReference = downloadManager.enqueue(request);
 					callbackContext.success(filename);
 				}
